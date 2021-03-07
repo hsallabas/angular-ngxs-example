e3e2ad23014ae80c4954187990bf331c14729b9a
hsallabas <sallabashalil@gmail.com>
2021-03-07 23:46:24 +0300

    Update theme toggle css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,16 @@ import { Component } from "@angular/core";
 @Component({
   selector: "my-app",
   template: `
-    <div class="wrapper" [ngClass]="{ 'dark-theme': darkMode }">
-      <div>
+    <div class="main" [ngClass]="{ 'dark-theme': darkMode }">
+      <div class="theme-toggle">
         <mat-slide-toggle [(ngModel)]="darkMode">
           Theme
         </mat-slide-toggle>
       </div>
-      <app-co2-add></app-co2-add>
-      <app-co2-list></app-co2-list>
+      <div class="wrapper">
+        <app-co2-add></app-co2-add>
+        <app-co2-list></app-co2-list>
+      </div>
     </div>
   `
 })
