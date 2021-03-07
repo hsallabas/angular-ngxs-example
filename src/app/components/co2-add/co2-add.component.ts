aef769a4bdfe1a74776880f66879f3cd377ea5e0
hsallabas <sallabashalil@gmail.com>
2021-03-07 22:34:29 +0300

    Update modal css

--- src/app/components/co2-add/co2-add.component.ts
+++ src/app/components/co2-add/co2-add.component.ts
@@ -66,7 +66,7 @@ export class Co2AddComponent implements OnInit, OnDestroy {
 
   selectEmoji() {
     const dialogRef = this.dialog.open(EmojiModalComponent, {
-      panelClass: 'dialog-style'
+      panelClass: "dialog-style"
     });
     dialogRef.afterClosed().subscribe(result => {
       if (result) {
