b18242382c930d4a13ffece220744cef3d5c7928
hsallabas <sallabashalil@gmail.com>
2021-03-07 16:19:24 +0300

    Create basic components

---
+++ src/app/components/co2-add/co2-add.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-co2-add",
+  templateUrl: "./co2-add.component.html",
+  styleUrls: ["./co2-add.component.scss"]
+})
+export class Co2AddComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ src/app/components/co2-list/co2-list.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-co2-list",
+  templateUrl: "./co2-list.component.html",
+  styleUrls: ["./co2-list.component.scss"]
+})
+export class Co2ListComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+  
+}
